747997c639f7e9f44f035999ee53ad49f5126253
Geoffry Millner <geoff.millner22@gmail.com>
2021-07-02 15:08:36 -0700

    make js objects that will hold data on places been and assign id to places added

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 function PlacesBeen() {
-    this.place = {};
+    this.placesList = {};
     this.currentId = 0;
 }
-
+// i want new items entered to  have new auto id such as place0 place2 place3
 PlacesBeen.prototype.addPlace = function(place) {
     place.id = this.assignId();
-    this.places[place.id] = place;
+    this.placesList[place.id] = place;
 }
 
 PlacesBeen.prototype.assignId = function() {
     this.currentId += 1;
     return this.currentId;
+}
+
+PlacesBeen.prototype.FindPlace = function(id) {
+    if (this.placesList[id] != undefined) {
+        return this.placesList[id];
+    }
+    return false;
 }
 
 // Business Logic down here
@@ -20,10 +27,20 @@ function Place(location, landmark, time, name) {
     this.time = time;
     this.name = name;
 }
+let placeData = new PlacesBeen();
+let myPlace1 = new Place("Oregon", "Highway I-84 East", "Summer", "Multnomah Falls" )//see line 5 above this would have a id place0
+let myPlace2 = new Place("Idaho", "Highway I-84 East", "Fall", "Pocatello" )// this would have id place1
+placeData.addPlace(myPlace1);
+placeData.addPlace(myPlace2);
 
-let myPlace1 = new Place("Oregon", "Highway I-84 East", "Summer", "Multnomah Falls" )
 // ideally user input above
+
 console.log(myPlace1.location)
+console.log(myPlace1.name)
+console.log(myPlace2.location)
+console.log(myPlace2.name)
+console.log(placeData.placesList[1])
+console.log(placeData.placesList[0])
 
 // I need to know if this is actually making new id if i were to ad more and more places like place 0 place 1 ect
 // i know that I need to use a submit function
